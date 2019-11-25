cb0f26c87887acdc592f75997f196a481acab63b
hehu <316031602@qq.com>
2019-11-25 17:02:46 +0800

    add session

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  github: {
+    client_id: '3acdc696c476a6a880d7',
+    client_secret: 'f339cc5c59e07a95a5bc1de46dfed8eafd49755b'
+  }
+}
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const Koa = require('koa')
 const KoaRouter = require('koa-router')
 const Next = require('next') // next作为koa中间件
+const session = require('koa-session')
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = Next({ dev })
@@ -10,7 +11,27 @@ app.prepare().then(() => {
   const server = new Koa()
   const router = new KoaRouter()
 
-  router.get('/page2/:id', async (ctx) => {
+  server.keys = ['tiger dev Github App']
+  const SESSION_CONFIG = {
+    key: 'jid'
+    // store: {}
+  }
+  server.use(session(SESSION_CONFIG, server))
+
+  server.use(async (ctx, next) => {
+    // console.log(ctx.cookies.get('id'))
+    if (!ctx.session.user) {
+      // ctx.session.user = {
+      //   name: 'tiger',
+      //   age: 25
+      // }
+    } else {
+      console.log('ctx.session === ', ctx.session)
+    }
+    await next()
+  })
+
+  router.get('/page2/:id', async ctx => {
     const id = ctx.params.id
     console.log('id === ', id)
     await handle(ctx.req, ctx.res, {
@@ -20,10 +41,19 @@ app.prepare().then(() => {
     ctx.response = false
   })
 
+  router.get('/set/user', async ctx => {
+    ctx.session.user = {
+      name: 'tiger',
+      age: 25
+    }
+    ctx.body = 'set session success'
+  })
+
   server.use(router.routes())
 
   // ctx.res是node的，ctx.response是koa封装过的 req同理
   server.use(async (ctx, next) => {
+    ctx.cookies.set('id', 'xxxxxx')
     await handle(ctx.req, ctx.res)
     ctx.response = false
   })
